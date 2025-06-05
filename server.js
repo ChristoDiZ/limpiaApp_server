affe11c9e21ce0dc2c0164beeb563b1eff2645cc
Christopher Diaz <christopher.diaz@psinet.cl>
2025-06-05 11:29:11 -0400

    forzar redeploy para solucionar crash

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
-const app=require('./app.js')
+require('dotenv').config();
+const app = require('./app');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Servidor esta en ejecucion ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor esta en ejecucion ${PORT}`);
+});
